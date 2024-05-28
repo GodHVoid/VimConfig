e4562cdc0b36e8691ad3bce9aa5205b099a53b99
GodHVoid <renatomillanx56@gmail.com>
2024-05-28 23:29:13 +0000

    Adding nvim configuration with lazy vim package

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbo,
+  { "scottmckendry/cyberdream.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "cyberdream",
+    },
+  },
+}
